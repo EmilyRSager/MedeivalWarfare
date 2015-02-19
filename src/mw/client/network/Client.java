5c0fee34e2fd86f375237513c2ffc6f130aae599
atg-abhishek <atg.abhishek@gmail.com>
2015-02-19 18:39:24 -0500

    revised client with reader and writer thread

--- src/mw/client/network/Client.java
+++ src/mw/client/network/Client.java
@@ -4,17 +4,17 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 import mw.server.gamelogic.Message;
 
-public class Client {
+public class Client extends Thread{
 	private static final String serverName = "localhost";
 	private static final int port = 6666;
 
-	private DataOutputStream aDataOutputStream;
-	private DataInputStream aDataInputStream;
-
 	private Socket aClientSocket;
+	
+	private Scanner reader = new Scanner(System.in);
 
 	public Client(){
 		try
@@ -26,56 +26,105 @@ public class Client {
 			
 			System.out.println("Just connected to "
 					+ aClientSocket.getRemoteSocketAddress());
-			aDataOutputStream = new DataOutputStream(aClientSocket.getOutputStream());
-			aDataInputStream = new DataInputStream(aClientSocket.getInputStream());
-			int i = 0;
-			while(i < 5){
-				sendString("Value of i = " + i + ".");
-				System.out.println("i = " + i++);
-			}
-
+			
+			
 		}catch(IOException e){
 			e.printStackTrace();
 		}
 	}
 
-	public void sendMessage(Message pMessage){
-		String s = pMessage.toJson();
-		try {
-			aDataOutputStream.writeUTF(s);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	class WriterThread extends Thread{
+		
+		DataOutputStream aDataOutputStream;
+		
+			
+		public void run(){
+			
+			try{
+				aDataOutputStream = new DataOutputStream(aClientSocket.getOutputStream());
+			
+				while(true){
+					
+					String lMessageToSend = reader.next();
+					
+					aDataOutputStream.writeUTF(lMessageToSend);
+					
+				}
+			
+			}
+			catch(Exception e){
+				System.out.println("in the catch block");
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	private class ReaderThread extends Thread{
+		DataInputStream aDataInputStream;
+		
+		
+		public void run(){
+			
+			try{
+				aDataInputStream = new DataInputStream(aClientSocket.getInputStream());
+			
+				while(true){
+					
+					String lMessageBeingRead = aDataInputStream.readUTF();
+					
+					System.out.println(lMessageBeingRead);
+					
+				}
+			
+			}
+			catch(Exception e){
+				System.out.println("in the catch block");
+				e.printStackTrace();
+			}
+			
 		}
 	}
+	
+	
+//	public void sendMessage(Message pMessage){
+//		String s = pMessage.toJson();
+//		try {
+//			aDataOutputStream.writeUTF(s);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 
 
-	public void readMessage(){
-		try {
-			Message m = Message.fromJson(aDataInputStream.readUTF());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+//	public void readMessage(){
+//		try {
+//			Message m = Message.fromJson(aDataInputStream.readUTF());
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 
-	public void sendString(String pString){
-		System.out.println("Sending \"" + pString + "\" to server.");
-		try {
-			aDataOutputStream.writeUTF(pString);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+//	public void sendString(String pString){
+//		System.out.println("Sending \"" + pString + "\" to server.");
+//		try {
+//			aDataOutputStream.writeUTF(pString);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 
-	public void readString(){
-		try {
-			System.out.println("Reading string from server.");
-			System.out.println(aDataInputStream.readUTF());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+//	public void readString(){
+//		try {
+//			System.out.println("Reading string from server.");
+//			System.out.println(aDataInputStream.readUTF());
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 }
