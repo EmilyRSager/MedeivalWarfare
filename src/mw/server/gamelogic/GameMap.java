aad7bfb61362a489ceb47fca60c1e7b40c5eaa8c
atg-abhishek <atg.abhishek@gmail.com>
2015-03-10 17:19:25 -0400

    added more logic to handle no village type

--- src/mw/server/gamelogic/GameMap.java
+++ src/mw/server/gamelogic/GameMap.java
@@ -81,7 +81,7 @@ public class GameMap  {
 				ArrayList<Tile> lNeighboringEmptyOrMeadowTiles = new ArrayList<Tile>();
 				for(Tile lTile: lTiles ){
 					StructureType lStructureType = lTile.getStructureType();
-					if (lStructureType.equals(StructureType.NO_STRUCT) || lStructureType.equals(StructureType.TREE) ) {
+					if (lStructureType.equals(StructureType.NO_STRUCT) || lStructureType.equals(StructureType.TREE) || lTile.getVillageType().equals(VillageType.NO_VILLAGE) ) {
 						lNeighboringEmptyOrMeadowTiles.add(lTile);
 					}
 				}
