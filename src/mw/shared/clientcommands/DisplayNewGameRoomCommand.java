f2518486e9dfda083a3fa5da91759f20199a6c09
Charles Bloomfield <charles.bloomfield@trusty>
2015-04-06 21:52:30 -0400

    fixed merge conflict

---
+++ src/mw/shared/clientcommands/DisplayNewGameRoomCommand.java
@@ -0,0 +1,19 @@
+package mw.shared.clientcommands;
+
+import mw.client.controller.menuing.MenuActionSender;
+import mw.shared.SharedCreatedGame;
+
+public class DisplayNewGameRoomCommand extends AbstractClientCommand {
+	private final String aType = "DisplayNewGameRoomCommand";
+	private SharedCreatedGame aNewGame;
+	
+	public DisplayNewGameRoomCommand(SharedCreatedGame pNewGame) {
+		aNewGame = pNewGame;
+	}
+	
+	@Override
+	public void execute() {
+		MenuActionSender.setGameCreationResult(aNewGame);
+	}
+
+}
