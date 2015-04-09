bfeb0d1e861ba514181cb7daff99bcfcd9941283
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-04-09 15:11:44 -0400

    removed useless empty lines

--- src/mw/client/app/ClientApplication.java
+++ src/mw/client/app/ClientApplication.java
@@ -6,12 +6,9 @@ import mw.client.controller.menuing.ScreenSwitcher.ScreenKind;
 import mw.client.network.NetworkController;
 
 public final class ClientApplication {
-
 	public static void main(String[] args)
 	{
 		NetworkController.initialize();
 		ScreenSwitcher.switchScreen(ScreenKind.LOGIN);
-		//NetworkDriver.main(args);
 	}
-	
 }
